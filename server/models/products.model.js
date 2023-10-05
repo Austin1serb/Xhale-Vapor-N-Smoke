0203369c4cb0415668265f598b207b2a6fb09284
Aaron <aaron.hsu86+github@gmail.com>
2023-10-05 12:47:19 -0700

    adding to product model

--- server/models/products.model.js
+++ server/models/products.model.js
@@ -8,15 +8,28 @@ const ProductsSchema = new mongoose.Schema({
     price: {
         type: Number,
         min: [0, "Price cannot be negative."],
-        required: [true, "Please provide product price."]
+        required: [true, "Please provide product price."],
+        // values are stored as cents in DB, but sent out as dollar values.
+        get: v => (v/100).toFixed(2),
+        set: v => v*100
     },
     imgSource: {
         type: String,
+    },
+    type: {
+        type: String,
+        // Vape, Catridge, Gummy, etc.
+        required: [true, "Please specify the type of product"]
     },
     description: {
         type: String,
         minlength: [2, "Description must be at least 2 character long."],
         maxlength: [750, "Description cannot be longer than 750 characters."]
+    },
+    size: {
+        type: Number,
+        min: [0, "Size cannot be negative."],
+        required: [true, "Please provide product size in mg"]
     }
 }, {timestamps:true})
 const Products = mongoose.model("Product", ProductsSchema)
