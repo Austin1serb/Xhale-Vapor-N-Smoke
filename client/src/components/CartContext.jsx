d849bdf77e2dc13974e775bc482d3cfd4a2b0eb1
Austin1serb <128577470+Austin1serb@users.noreply.github.com>
2023-11-21 08:23:42 -0800

    nov 21st

--- client/src/components/CartContext.jsx
+++ client/src/components/CartContext.jsx
@@ -1,6 +1,5 @@
 import React, { createContext, useState, useContext } from 'react';
-import { Snackbar, Button, IconButton } from '@mui/material';
-import { FcCheckmark } from 'react-icons/fc';
+import { Snackbar } from '@mui/material';
 
 import Confetti from 'react-confetti';
 const CartContext = createContext();
@@ -71,7 +70,11 @@ export const CartProvider = ({ children }) => {
                 autoHideDuration={2000} // Snackbar will close after 6 seconds
                 onClose={() => setOpenSnackbar(false)}
                 message={`${name} has been successfully added to the cart.`}
-                action={<React.Fragment> <FcCheckmark style={{ fontSize: 26 }} /></React.Fragment>}
+                action={<React.Fragment>
+                    <svg height='24' version="1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 48 48" enable-background="new 0 0 48 48">
+                        <polygon fill="#43A047" points="40.6,12.1 17,35.7 7.4,26.1 4.6,29 17,41.3 43.4,14.9" />
+                    </svg>
+                </React.Fragment>}
             />
 
         </CartContext.Provider>
